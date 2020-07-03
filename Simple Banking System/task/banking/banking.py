7a5055274b27fe7655c15e78446f340e85b4655c
Sergey Gurman <sgurman753@gmail.com>
2020-07-03 16:20:47 -0400

    understanding dictionary

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -1,1 +1,22 @@
-# Write your code here
+import random
+
+
+class Bank:
+    BIN = 400000
+    checksum = 5
+
+    def __init__(self):
+        self.account_identifier = random.randint(0, int('9' * 9))
+        self.card_number = int(str(Bank.BIN) + str(self.account_identifier) + str(Bank.checksum))
+        self.pin = random.randint(0, 9999)
+
+
+if __name__ == "__main__":
+    a = Bank()
+    b = Bank()
+    print(a.card_number, a.pin)
+    print(b.card_number, b.pin)
+    print(Bank().card_number)
+    print(Bank().card_number)
+    print(Bank().card_number)
+    print(a.card_number, a.pin)
