f595a9d1daf7f168dd95e6ffc7b2af7dc7218e48
Sergey Gurman <sgurman753@gmail.com>
2020-07-03 21:17:32 -0400

    added a print_menu function

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -4,22 +4,38 @@ import random
 class Bank:
     BIN = 400000
     checksum = 5
+    accounts = {}
 
     def __init__(self):
         self.account_identifier = Bank.add_zeros(random.randint(0, int('9' * 9)), 9)
-        self.card_number = int(str(Bank.BIN) + str(self.account_identifier) + str(Bank.checksum))
+        self.card_number = int(str(Bank.BIN) + self.account_identifier + str(Bank.checksum))
         self.pin = random.randint(0, 9999)
+        Bank.accounts.update({self.pin: self.card_number})
 
     @staticmethod
     def add_zeros(num, length):
         if len(str(num)) < length:
             missing_zero = "0" * (length - len(str(num)))
             return missing_zero + str(num)
-        return num
+        return str(num)
+
+
+def print_menu(menu):
+    for key, value in menu.items():
+        print(f"{key}. {value}")
 
 
 if __name__ == "__main__":
-    a = Bank()
-    b = Bank()
-    print(a.card_number, a.pin)
-    print(b.card_number, b.pin)
+    starting_menu = {1: "Create an account", 2: "Log into account", 0: "Exit"}
+    logging_in_menu = {1: "Balance", 2: "Log out", 0: "Exit"}
+
+    action = None
+    while not action:
+        print_menu(starting_menu)
+        action = input()
+        if action == 1:
+            pass
+        elif action == 2:
+            pass
+
+        action = True
