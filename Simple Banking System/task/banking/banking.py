739161c0e3b75cfb4e2909f766430ff741c066f8
Sergey Gurman <sgurman753@gmail.com>
2020-07-03 20:20:21 -0400

    added a method that add zero to a number

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -6,9 +6,16 @@ class Bank:
     checksum = 5
 
     def __init__(self):
-        self.account_identifier = random.randint(0, int('9' * 9))
+        self.account_identifier = Bank.add_zeros(random.randint(0, int('9' * 9)), 9)
         self.card_number = int(str(Bank.BIN) + str(self.account_identifier) + str(Bank.checksum))
         self.pin = random.randint(0, 9999)
+
+    @staticmethod
+    def add_zeros(num, length):
+        if len(str(num)) < length:
+            missing_zero = "0" * (length - len(str(num)))
+            return missing_zero + str(num)
+        return num
 
 
 if __name__ == "__main__":
@@ -16,9 +23,3 @@ if __name__ == "__main__":
     b = Bank()
     print(a.card_number, a.pin)
     print(b.card_number, b.pin)
-    print(Bank().card_number)
-    print(Bank().card_number)
-    print(Bank().card_number)
-    print(a.card_number, a.pin)
-
-    print()
