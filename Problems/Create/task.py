09644c30dd20821e57ecfb4ffa754a0e0247439b
Sergey Gurman <sgurman753@gmail.com>
2020-07-10 13:16:15 -0400

    problem of the day

---
+++ Problems/Create/task.py
@@ -0,0 +1,7 @@
+# create a dict here
+favorite = {"Alex": "field flowers",
+            "Kate": "daffodil",
+            "Eva": "artichoke flower",
+            "Daniel": "tulip"}
+# and print it
+print(favorite)
